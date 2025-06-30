0d2294611fa197d855a8433eb260ef74b667734f
thanadon.s <thanadonsaojarkaval@gmail.com>
2025-06-30 15:45:56 +0700

    - connect to mongodb

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"user-manages/config"
+	"user-manages/pkg/database"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Initialize config
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		}
+		return os.Args[1]
+	}())
+
+	// Database connection
+	db := database.DbConn(ctx, &cfg)
+	defer db.Disconnect(ctx)
+}
