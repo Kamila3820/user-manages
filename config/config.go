3c03a3b723743ec40c3c00dc799037d482ad537b
thanadon.s <thanadonsaojarkaval@gmail.com>
2025-06-30 15:34:40 +0700

    - config: Load config from env

---
+++ config/config.go
@@ -0,0 +1,82 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+type (
+	Config struct {
+		App  App
+		Db   Db
+		Jwt  Jwt
+		Grpc Grpc
+	}
+
+	App struct {
+		Name  string
+		Url   string
+		Stage string
+	}
+
+	Db struct {
+		Url string
+	}
+
+	Jwt struct {
+		AccessSecretKey  string
+		RefreshSecretKey string
+		ApiSceretKey     string
+		AccessDuration   int64
+		RefreshDuration  int64
+		ApiDuration      int64
+	}
+
+	Grpc struct {
+		AuthUrl   string
+		PlayerUrl string
+	}
+)
+
+func LoadConfig(path string) Config {
+	if err := godotenv.Load(path); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return Config{
+		App: App{
+			Name:  os.Getenv("APP_NAME"),
+			Url:   os.Getenv("APP_URL"),
+			Stage: os.Getenv("APP_STAGE"),
+		},
+		Db: Db{
+			Url: os.Getenv("DB_URL"),
+		},
+		Jwt: Jwt{
+			AccessSecretKey:  os.Getenv("JWT_ACCESS_SECRET_KEY"),
+			RefreshSecretKey: os.Getenv("JWT_REFRESH_SECRET_KEY"),
+			ApiSceretKey:     os.Getenv("JWT_API_SECRET_KEY"),
+			AccessDuration: func() int64 {
+				result, err := strconv.ParseInt(os.Getenv("JWT_ACCESS_DURATION"), 10, 64)
+				if err != nil {
+					log.Fatal("Error loading access duration failed")
+				}
+				return result
+			}(),
+			RefreshDuration: func() int64 {
+				result, err := strconv.ParseInt(os.Getenv("JWT_REFRESH_DURATION"), 10, 64)
+				if err != nil {
+					log.Fatal("Error loading refresh duration failed")
+				}
+				return result
+			}(),
+		},
+		Grpc: Grpc{
+			AuthUrl:   os.Getenv("GRPC_AUTH_URL"),
+			PlayerUrl: os.Getenv("GRPC_PLAYER_URL"),
+		},
+	}
+}
